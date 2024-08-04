6720e7c6380dcd87a8b953aa9eb7cd2870b9bdfd
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-04 19:15:38 -0300

    feat: create a custom exception class

---
+++ backend/src/Exceptions/CustomException.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use Exception;
+
+
+class CustomException extends Exception
+{
+    protected array $errors;
+
+    public function __construct($message, $errors)
+    {
+        parent::__construct($message);
+        $this->errors = $errors;
+    }
+
+
+    public function getErrorsMessage(): array
+    {
+        return $this->errors;
+    }
+}
+
+

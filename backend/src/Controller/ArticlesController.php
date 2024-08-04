aabdc76c0cfde6f7769392590e9294ac87ee1e43
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-04 20:41:38 -0300

    feat: config controller to working with json

--- backend/src/Controller/ArticlesController.php
+++ backend/src/Controller/ArticlesController.php
@@ -3,9 +3,23 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Cake\View\JsonView;
+
 
 class ArticlesController extends AppController
 {
+
+    public function initialize(): void
+    {
+        parent::initialize();
+    }
+
+
+    public function viewClasses(): array
+    {
+        return [JsonView::class];
+    }
+
 
     public function add()
     {

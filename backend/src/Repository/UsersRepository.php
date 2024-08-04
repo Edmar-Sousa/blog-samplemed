802eda40e8f921d66b5f327f9d72855b8ad0487d
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-04 10:50:16 -0300

    feat: create user repository and custom validate exception to comunication from repository and controller

---
+++ backend/src/Repository/UsersRepository.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Model\Entity\User;
+use App\Model\Table\UsersTable;
+use App\Exceptions\ValidationException;
+
+
+
+class UsersRepository
+{
+    protected UsersTable $usersTable;
+
+    public function __construct(UsersTable $usersTable)
+    {
+        $this->usersTable = $usersTable;
+    }
+
+
+
+    public function saveUser(array $userData): User
+    {
+        $userEntity = $this->usersTable->newEntity($userData);
+
+        if (!$this->usersTable->save($userEntity))
+            throw new ValidationException('Erro ao cadastrar usuario', $userEntity->getErrors());
+
+        return $userEntity;
+    }
+}

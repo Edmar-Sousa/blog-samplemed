160eb9ff2a617c220e8720be448e9de0ea5bc3b9
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-04 10:50:34 -0300

    feat: create logic to store new user

--- backend/src/Controller/UsersController.php
+++ backend/src/Controller/UsersController.php
@@ -3,11 +3,23 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Exceptions\ValidationException;
+use App\Repository\UsersRepository;
 use Cake\View\JsonView;
+use Exception;
 
 
 class UsersController extends AppController
 {
+
+    protected UsersRepository $usersRepository;
+
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        $this->usersRepository = new UsersRepository($this->Users);
+    }
 
     public function viewClasses(): array
     {
@@ -20,14 +32,37 @@ class UsersController extends AppController
         $this->request->allowMethod(['post']);
 
 
-        $users = [
-            'name' => 'Edinho',
-            'email' => 'email@gmail.com',
-        ];
+        try {
+
+            $userData = $this->request->getData();
+            $user = $this->usersRepository->saveUser($userData);
+
+            $this->response = $this->response->withStatus(201);
+
+            $this->set('user', $user);
+            $this->viewBuilder()->setOption('serialize', ['user']);
+        } catch (Exception $err) {
+
+            $error = [
+                'message' => 'Erro interno do servidor',
+            ];
+
+            $this->response = $this->response->withStatus(500);
 
 
-        $this->set('user', $users);
-        $this->viewBuilder()->setOption('serialize', ['user']);
+            if ($err instanceof ValidationException) {
+                $error = [
+                    'message' => $err->getMessage(),
+                    'details' => $err->getValidationMessages()
+                ];
+
+                $this->response = $this->response->withStatus(400);
+            }
+
+            $this->set('error', $error);
+            $this->viewBuilder()->setOption('serialize', ['error']);
+        }
+
 
     }
 

6ea21626ccc8bcd7b576a4975a5da60bad0257ab
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-04 20:55:05 -0300

    feat: method to store article in database

--- backend/src/Controller/ArticlesController.php
+++ backend/src/Controller/ArticlesController.php
@@ -3,15 +3,20 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\ArticlesRepository;
 use Cake\View\JsonView;
 
 
 class ArticlesController extends AppController
 {
 
+    protected ArticlesRepository $articlesRepository;
+
+
     public function initialize(): void
     {
         parent::initialize();
+        $this->articlesRepository = new ArticlesRepository($this->Articles);
     }
 
 
@@ -23,7 +28,23 @@ class ArticlesController extends AppController
 
     public function add()
     {
+        $articleData = $this->request->getData();
 
+        $article = $this->articlesRepository->saveArticle($articleData);
+
+        $locationHeader = url([
+            'action' => 'view',
+            'id' => $article->id,
+            '_ext' => 'json',
+            'fullBase' => true
+        ]);
+
+
+        $this->response = $this->response->withStatus(201)
+            ->withAddedHeader('Location', $locationHeader);
+
+        $this->set('article', $article);
+        $this->viewBuilder()->setOption('serialize', ['article']);
     }
 
 }

93e0b37beb4cb9700d093aa0260d7124c7a61995
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-04 19:09:15 -0300

    feat: exception to cominicate the controller

---
+++ backend/src/Exceptions/InvalidPasswordException.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use Exception;
+
+
+class InvalidPasswordException extends Exception
+{
+
+    protected array $errors;
+
+    public function __construct(string $message, $errors)
+    {
+        parent::__construct($message);
+
+        $this->errors = $errors;
+    }
+
+
+
+    public function getErrorsMessages()
+    {
+        return $this->errors;
+    }
+}

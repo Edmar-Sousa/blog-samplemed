523dba67a0d05171259b0e1f057de13b28c67fa9
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-04 21:07:37 -0300

    feat: handler errors

--- backend/src/Controller/ArticlesController.php
+++ backend/src/Controller/ArticlesController.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Repository\ArticlesRepository;
+use App\Exceptions\ValidationException;
+use Exception;
 use Cake\View\JsonView;
+use App\Repository\ArticlesRepository;
 
 
 class ArticlesController extends AppController
@@ -28,23 +30,46 @@ class ArticlesController extends AppController
 
     public function add()
     {
-        $articleData = $this->request->getData();
+        try {
 
-        $article = $this->articlesRepository->saveArticle($articleData);
+            $articleData = $this->request->getData();
 
-        $locationHeader = url([
-            'action' => 'view',
-            'id' => $article->id,
-            '_ext' => 'json',
-            'fullBase' => true
-        ]);
+            $article = $this->articlesRepository->saveArticle($articleData);
+
+            $locationHeader = url([
+                'action' => 'view',
+                'id' => $article->id,
+                '_ext' => 'json',
+                'fullBase' => true
+            ]);
 
 
-        $this->response = $this->response->withStatus(201)
-            ->withAddedHeader('Location', $locationHeader);
+            $this->response = $this->response->withStatus(201)
+                ->withAddedHeader('Location', $locationHeader);
 
-        $this->set('article', $article);
-        $this->viewBuilder()->setOption('serialize', ['article']);
+            $this->set('article', $article);
+            $this->viewBuilder()->setOption('serialize', ['article']);
+        } catch (Exception $err) {
+            $error = [
+                'message' => 'Erro interno no servidor'
+            ];
+
+            $httpStatusCode = 500;
+
+            if ($err instanceof ValidationException) {
+                $error = [
+                    'message' => $err->getMessage(),
+                    'details' => $err->getErrorsMessage(),
+                ];
+
+                $httpStatusCode = 400;
+            }
+
+            $this->response = $this->response->withStatus($httpStatusCode);
+
+            $this->set('error', $error);
+            $this->viewBuilder()->setOption('serialize', ['error']);
+        }
     }
 
 }

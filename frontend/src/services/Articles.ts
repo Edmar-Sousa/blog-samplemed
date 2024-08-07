355f776145246902565c8545c3b916426c3bb29f
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-06 22:21:20 -0300

    feat: function to get articles

---
+++ frontend/src/services/Articles.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+import { ArticleType } from '../interfaces/articles'
+import { http } from '../http/axios'
+
+
+
+export async function getLatestArticles(): Promise<Array<ArticleType>> {
+    try {
+        const response = await http.get<Array<ArticleType>>('/api/articles.json', {
+            params: {
+                page: 1,
+            }
+        })
+
+
+        return response.data
+    }
+
+    catch (err) {
+
+        if (axios.isAxiosError(err))
+            throw new Error(err.response?.data.message)
+
+        throw new Error('Erro ao buscar postagens')
+    }
+}
+

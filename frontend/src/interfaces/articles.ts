5088eef7fb690ef32a2ff5f318d58c5691b97193
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-06 21:28:00 -0300

    feat: define article typr

---
+++ frontend/src/interfaces/articles.ts
@@ -0,0 +1,13 @@
+import { UserType } from './user'
+
+
+export interface ArticleType {
+    id: string
+    title: string
+    banner_image: string
+    content: string
+    created: string
+    modified: string
+    user: UserType
+    user_id: string
+}

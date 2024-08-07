5272a5267eabe95a5ffd7022d29b94230545bdf7
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-06 23:45:09 -0300

    feat: input text component

---
+++ frontend/src/components/TextInput.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <float-label class="relative block">
+        <input-text 
+            :type="type"
+            :id="id"
+            class="float-label-input w-full h-[65px] border-2 border-gray-100 rounded-[10px] font-nunito px-5 text-gray-300 text-lg font-semibold outline-none focus:ring focus:ring-green-300"
+            v-model="model" />
+
+        <label 
+            :for="id"
+            class="font-semibold font-nunito text-gray-300 text-lg absolute top-5 left-4 duration-500 px-1 pointer-events-none">
+                {{ placeholder }}
+        </label>
+    </float-label>
+
+    <small 
+        v-if="error"
+        class="font-nunito text-red-600 text-base mt-1">
+            {{ error }}
+    </small>
+
+</template>
+
+<script setup lang="ts">
+
+
+import InputText from 'primevue/inputtext'
+import FloatLabel from 'primevue/floatlabel'
+
+defineProps([
+    'id',
+    'placeholder',
+    'error',
+    'type'
+])
+
+const model = defineModel()
+
+</script>

eacdfd1869ef8f8cbde611835f05b2911b1e8b7d
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-06 22:27:09 -0300

    refact: put header in app component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,11 @@
 <template>
 
+    <header class="h-[80px] w-full">
+        <nav class="w-full max-w-[1100px] h-[80px] mx-auto flex items-center">
+            <h1 class="font-nunito text-3xl">Blog Semplemed</h1>
+        </nav>
+    </header>
+
     <router-view />
 
 </template>

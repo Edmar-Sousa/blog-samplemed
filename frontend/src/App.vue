f6bda0117163d470b1564a496de4020661182f59
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-06 23:03:44 -0300

    fix: add horizontal padding in header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
 
-    <header class="h-[80px] w-full">
-        <nav class="w-full max-w-[1100px] h-[80px] mx-auto flex items-center">
+    <header class="h-[80px] w-full px-4">
+        <nav class="w-full max-w-[1400px] h-[80px] mx-auto flex items-center">
             <h1 class="font-nunito text-3xl">Blog Semplemed</h1>
         </nav>
     </header>

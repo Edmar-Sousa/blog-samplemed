dade70b3444de2bb875fdaf5615384107562694a
Edmar_Sousa <edmarsousamendes4@gmail.com>
2024-08-07 00:40:37 -0300

    feat: create seed to one user

---
+++ backend/config/Seeds/UsersSeed.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+use Cake\Utility\Text;
+use Migrations\AbstractSeed;
+use Authentication\PasswordHasher\DefaultPasswordHasher;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'id' => Text::uuid(),
+                'name' => 'Usuario Padrao',
+                'username' => 'usuario_padrao',
+                'email' => 'usuariopadrao@gmail.com',
+                'password' => (new DefaultPasswordHasher)->hash('123'),
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
